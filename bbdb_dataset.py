694ef1c4235edfa608762a1fdaf6c0127db32483
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2020-01-06 17:22:37 -0500

    Add stub file for BBDBDataset

---
+++ bbdb_dataset.py
@@ -0,0 +1,19 @@
+"""
+Define the PyTorch Dataset class for Baseball Database (BBDB).
+"""
+
+from torch.utils.data import Dataset
+
+
+class BBDBDataset(Dataset):
+    """
+    PyTorch Dataset class for Baseball Database (BBDB).
+    """
+    def __init__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        pass
